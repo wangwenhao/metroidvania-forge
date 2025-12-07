81fcf24f24cad1eafa575052944ff1753130c195
王闻昊 <wangwenhao@live.cn>
2025-12-07 13:11:11 +0800

    场景管理与切换

--- general/level_transition/level_transition.gd
+++ general/level_transition/level_transition.gd
@@ -3,7 +3,7 @@
 class_name LevelTransition extends Node2D
 
 enum SIDE {
-	LEFT, RIGHT, UP, DOWN
+	LEFT, RIGHT, TOP, BOTTOM
 }
 
 @export_range(2, 16, 1, "or_greater") var size: int = 2:
@@ -21,6 +21,32 @@ enum SIDE {
 
 @onready var area: Area2D = $Area2D
 
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
+	SceneManager.new_scene_ready.connect(_on_new_scene_ready)
+	SceneManager.load_scene_finished.connect(_on_load_scene_finished)
+
+
+func _on_player_entered(player: Node2D) -> void:
+	SceneManager.transition_scene(target_level, target_area_name, get_offset(player), get_transition_direction())
+
+
+func _on_new_scene_ready(target_name: String, offset: Vector2) -> void:
+	if target_name == name:
+		var player := get_tree().get_first_node_in_group("Player")
+		player.global_position = global_position + offset
+	pass
+
+func _on_load_scene_finished() -> void:
+	area.monitoring = false
+	area.body_entered.connect(_on_player_entered)
+	await get_tree().physics_frame
+	await get_tree().physics_frame
+	area.monitoring = true
+
+
 func apply_area_settings() -> void:
 	area = get_node_or_null("Area2D")
 	if not area:
@@ -34,7 +60,39 @@ func apply_area_settings() -> void:
 			area.scale.x = 1
 	else:
 		area.scale.x = size
-		if location == SIDE.UP:
+		if location == SIDE.TOP:
 			area.scale.y = 1
 		else:
 			area.scale.y = -1
+
+
+func get_offset(player: Node2D) -> Vector2:
+	var offset: Vector2 = Vector2.ZERO
+	var player_position = player.global_position
+	
+	if location == SIDE.LEFT or location == SIDE.RIGHT:
+		offset.y = player_position.y - self.global_position.y
+		if location == SIDE.LEFT:
+			offset.x = -12
+		else:
+			offset.x = 12
+	else:
+		offset.x = player_position.x - self.global_position.x
+		if location == SIDE.TOP:
+			offset.y = -2
+		else:
+			offset.y = 48
+	
+	return offset
+
+
+func get_transition_direction() -> String:
+	match location:
+		SIDE.LEFT:
+			return "left"
+		SIDE.RIGHT:
+			return "right"
+		SIDE.TOP:
+			return "up"
+		_:
+			return "down"
--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -30,7 +30,10 @@ var gravity_multiplier: float = 1
 #endregion
 
 func _ready() -> void:
+	if get_tree().get_first_node_in_group("Player") != self:
+		self.queue_free()
 	initialize_state()
+	self.call_deferred("reparent", get_tree().root)
 	
 func _unhandled_input(event: InputEvent) -> void:
 	change_state(current_state.handle_input(event))
